cc910be5d76853bb00e00270e233e4e51ef91d1b
sachin61195 <sachinkumar@cedcommerce.com>
2022-03-10 15:07:44 +0530

    some changes

--- JS/task 1/index.js
+++ JS/task 1/index.js
@@ -4,7 +4,7 @@ function myfun(){
     document.getElementById("fname").innerHTML = fname;
     let name = `${fname} ${lname}`;
     let letters= name.length;
-    let subtotal = letters*5;
+    let subtotal = letters*5 ;
     let shipping = 7;
     let grandtotal = subtotal+shipping;
     document.getElementById("name").innerHTML = fname+" "+lname;
